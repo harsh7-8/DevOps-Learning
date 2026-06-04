409c160c8e73da69a4b86a8b5a4fd2b3793b5cc6
harsh7-8 <harshadchahahre@gmail.com>
2026-06-04 08:10:30 +0530

    Modified test shell scripts

--- shell-scripts/test.sh
+++ shell-scripts/test.sh
@@ -1,1 +1,15 @@
 echo Hello World!
+
+echo \\e[em30Hello World\\e[0m
+echo \\e[em31Hello World\\e[0m
+echo \\e[em32Hello World\\e[0m
+echo \\e[em33Hello World\\e[0m
+echo \\e[em34Hello World\\e[0m
+echo \\e[em35Hello World\\e[0m
+
+echo \\e[em40Hello World\\e[0m
+echo \\e[em41Hello World\\e[0m
+echo \\e[em42Hello World\\e[0m
+echo \\e[em43Hello World\\e[0m
+echo \\e[em44Hello World\\e[0m
+echo \\e[em45Hello World\\e[0m
