6478de44faf863436371687ff6620d4a2d464770
harsh7-8 <harshadchahahre@gmail.com>
2026-06-13 20:17:38 +0530

    Added redirector

--- shell-scripts/redirector.sh
+++ shell-scripts/redirector.sh
@@ -1,3 +1,7 @@
 # 1> Redirect stdout to a file (overwrite)
 echo "This is a test message" 1> /tmp/output.txt
 echo "This is second test message" 1> /tmp/output.txt
+
+# 1>> Redirect stdout to a file (Append)
+echo "This is a test message" 1>/tmp/output1.txt
+echo "This is second test message" 1>> /tmp/output1.txt
