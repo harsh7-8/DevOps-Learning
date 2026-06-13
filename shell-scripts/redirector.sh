350e22c1c5436d32af16849827cafd0a8fe707da
harsh7-8 <harshadchahahre@gmail.com>
2026-06-13 20:25:59 +0530

    Added redirector

--- shell-scripts/redirector.sh
+++ shell-scripts/redirector.sh
@@ -5,3 +5,6 @@ echo "This is second test message" 1> /tmp/output.txt
 # 1>> Redirect stdout to a file (Append)
 echo "This is a test message" 1>/tmp/output1.txt
 echo "This is second test message" 1>> /tmp/output1.txt
+
+ls /tmp/ > /tmp/output.txt
+ls /tmp2/ 2> /tmp/error.txt
