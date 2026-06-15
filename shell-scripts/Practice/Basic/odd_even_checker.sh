212e61089ba02e751f0d3f9506b7b891a59ac709
harsh7-8 <harshadchahahre@gmail.com>
2026-06-15 22:09:57 +0530

    Adding practice shell problem

--- shell-scripts/Practice/Basic/odd_even_checker.sh
+++ shell-scripts/Practice/Basic/odd_even_checker.sh
@@ -1,6 +1,10 @@
+#Arithmetic evaluation (( )) → use ==
+#Here, ==, !=, >, <, >=, <= are used naturally.
+#2. Test command [ ] → use -eq
+
 read -p "Enter a number:" num
 
-if (( num % 2 = 0 )); then
+if (( num % 2 == 0 )); then
     echo "The number is even."
 else
     echo "The number is odd."
