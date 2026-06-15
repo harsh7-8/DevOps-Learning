a37f92a756e2074d393e3c8e534c6a94b5b3873d
harsh7-8 <harshadchahahre@gmail.com>
2026-06-16 00:23:27 +0530

    Adding practice shell problem

--- shell-scripts/Practice/Basic/create_del_user.sh
+++ shell-scripts/Practice/Basic/create_del_user.sh
@@ -10,26 +10,36 @@ read -p "Enter your choice (1/2/3):" choice
 case $choice in
     1)
         read -p "Enter username to create:" username
+        wait 2
         sudo useradd $username
+        wait 1
         echo "User $username created successfully."
         ;;
     2)
         read -p "Enter username to create (no login):" username
+        wait 2
         sudo useradd -s /usr/sbin/nologin $username
+        wait 1
         echo "No login user $username created successfully."
         ;;
     3)
         read -p "Enter username to delete:" username
+        wait 2
         if id "$username" &>/dev/null; then
             echo "User $username exists. Deleting..."
+            wait 2
         else 
             echo "User $username does not exist."
+            wait 1
             exit 1
         fi
+        wait 1
         sudo userdel $username
+        wait 1
         echo "User $username deleted successfully."
         ;;
     *)
+        wait 1
         echo "Invalid choice. Please select 1, 2, or 3."
         ;;
 esac
