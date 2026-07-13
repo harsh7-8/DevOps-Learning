48a8897fc0d89316c1a5f5d7be967df66c985a0e
harsh7-8 <harshadchahahre@gmail.com>
2026-07-13 09:31:41 +0530

    modified shell scripts

--- WMP-shell-scripting/portfolio-service.sh
+++ WMP-shell-scripting/portfolio-service.sh
@@ -22,7 +22,7 @@ status_check
 set_permissions
 
 echo -e "${YC}Copy Service File${NC}"
-echo $pwd
+echo pwd
 cp ${service_name}.service /etc/systemd/system/${service_name}.service &>>$OUTPUT
 status_check
 
